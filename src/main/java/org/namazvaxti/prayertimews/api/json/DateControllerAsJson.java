2ef3adef9d181abdb52d6e01d29204c3ae0c3254
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2021-10-13 17:52:29 +0400

    DateControllerAsJson is completed

--- src/main/java/org/namazvaxti/prayertimews/api/json/DateControllerAsJson.java
+++ src/main/java/org/namazvaxti/prayertimews/api/json/DateControllerAsJson.java
@@ -9,7 +9,6 @@ import org.namazvaxti.prayertimews.core.utilities.result.error.ErrorDataResult;
 import org.namazvaxti.prayertimews.entities.concretes.CityBean;
 import org.namazvaxti.prayertimews.entities.concretes.time.City;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
-import javax.json.JsonStructure;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
@@ -27,8 +25,8 @@ import java.util.*;
 @RequestMapping("api/dates/json/1.0")
 public class DateControllerAsJson {
 
-    private TimeService timeService;
-    private ClientServer clientServer;
+    private final TimeService timeService;
+    private final ClientServer clientServer;
 
     @Autowired
     public DateControllerAsJson(ClientServer clientServer , TimeService timeService) {
@@ -55,11 +53,12 @@ public class DateControllerAsJson {
 
 
     @SuppressWarnings("unchecked")
-    @GetMapping("/todaydate/{indexOfCity}")
-    public ResponseEntity<DataResult<City>> getDatesOfToDay(@Param("indexOfCity") Integer indexOfCity) throws BaseException {
+    @GetMapping("/todaydate/")
+    public ResponseEntity<DataResult<City>> getDatesOfToDay(@RequestParam("indexOfCity") Integer indexOfCity) throws BaseException {
         return new ResponseEntity<DataResult<City>>(this.timeService.getDatesOfDay(indexOfCity),HttpStatus.OK);
     }
 
+    @SuppressWarnings("unchecked")
     @PostMapping("/currentdate")
     public ResponseEntity<DataResult<City>> getDatesOfCurrentDay(@RequestParam("indexOfCity") Integer indexOfCity,
                                                                  @RequestParam("date") @DateTimeFormat(pattern = "MM-dd-yyyy") LocalDate date) throws BaseException {
@@ -67,14 +66,15 @@ public class DateControllerAsJson {
     }
 
     @SuppressWarnings("unchecked")
-    @GetMapping("/weeklydates/{indexOfCity}")
-    public ResponseEntity<DataResult<Map<Integer,City>>> getWeeklyDates(@Param("indexOfCity") Integer indexOfCity) throws BaseException {
+    @GetMapping("/weeklydates/")
+    public ResponseEntity<DataResult<Map<Integer,City>>> getWeeklyDates(@RequestParam("indexOfCity") Integer indexOfCity) throws BaseException {
         return new ResponseEntity<DataResult<Map<Integer,City>>>(this.timeService.getWeeklyDates(indexOfCity),HttpStatus.OK);
     }
 
 
-    @GetMapping("/monthlydates/{indexOfCity}")
-    public ResponseEntity<DataResult<Map<Integer,City>>> getMonthlyDates(@Param("indexOfCity") Integer indexOfCity) throws BaseException {
+    @SuppressWarnings("unchecked")
+    @GetMapping("/monthlydates/")
+    public ResponseEntity<DataResult<Map<Integer,City>>> getMonthlyDates(@RequestParam("indexOfCity") Integer indexOfCity) throws BaseException {
         return new ResponseEntity<DataResult<Map<Integer,City>>>(this.timeService.getMonthDates(indexOfCity),HttpStatus.OK);
     }
 
@@ -83,26 +83,26 @@ public class DateControllerAsJson {
 
     @ExceptionHandler(IndexOfCityNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDataResult<Object> indexOfCityNotFoundException(IndexOfCityNotFoundException exception){
-        return new ErrorDataResult<Object>(ErrorMessages.INVALID_INDEX.getValue());
+    public ErrorDataResult<Object> indexOfCityNotFoundException(){
+        return new ErrorDataResult<>(ErrorMessages.INVALID_INDEX.getValue());
     }
 
     @ExceptionHandler(NullValueException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDataResult<Object> nullValueException(NullValueException exception){
-        return new ErrorDataResult<Object>(ErrorMessages.NULL_VALUE.getValue());
+    public ErrorDataResult<Object> nullValueException(){
+        return new ErrorDataResult<>(ErrorMessages.NULL_VALUE.getValue());
     }
 
     @ExceptionHandler(DataNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDataResult<Object> dataNotFoundException(DataNotFoundException exception){
-        return new ErrorDataResult<Object>(ErrorMessages.DATA_NOT_FOUND.getValue());
+    public ErrorDataResult<Object> dataNotFoundException(){
+        return new ErrorDataResult<>(ErrorMessages.DATA_NOT_FOUND.getValue());
     }
 
     @ExceptionHandler(UnknownException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDataResult<Object> unknowException(UnknownException exception){
-        return new ErrorDataResult<Object>(ErrorMessages.UNKNOWN.getValue());
+    public ErrorDataResult<Object> unknowException(){
+        return new ErrorDataResult<>(ErrorMessages.UNKNOWN.getValue());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -112,7 +112,7 @@ public class DateControllerAsJson {
         for (FieldError fieldError : exception.getBindingResult().getFieldErrors()){
             validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
         }
-        return new ErrorDataResult<Object>(validationErrors,ErrorMessages.UNKNOWN.getValue());
+        return new ErrorDataResult<>(validationErrors,ErrorMessages.UNKNOWN.getValue());
     }
 
 }
